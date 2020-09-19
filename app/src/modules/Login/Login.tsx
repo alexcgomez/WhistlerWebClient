d87b464582aa0b21c9d2bad9fb7c691ea768d39a
Angel Andreu <txelut@gmail.com>
2020-09-19 04:05:27 +0200

    Merged changes in Login and fixed Register button.

--- app/src/modules/Login/Login.tsx
+++ app/src/modules/Login/Login.tsx
@@ -16,60 +16,62 @@ function Login() {
   const dispatch = useDispatch();
   const history = useHistory();
 
- async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     setSubmitted(true);
-    dispatch(userLogin(email, password,history));
+    dispatch(userLogin(email, password, history));
   }
 
   return (
     <div className="login-container">
-        <Logo maxWidth="250px"/>
-      {submitted && authentication.error &&
-        <div className="alert alert-danger" role="alert">
-        Email or password is incorrect.
-        </div>}
-        <form onSubmit={(e) => handleSubmit(e)}>
+      <Logo maxWidth="250px" />
+      <form onSubmit={(e) => handleSubmit(e)}>
         <div className="form-group">
-        <label>Email</label>
-        <input
-        type="email"
-        className="form-control form-control-sm"
-        aria-describedby="emailHelp"
-        placeholder="name@example.com"
-        onChange={(e) => setEmail(e.target.value)}
-        required
-        />
-        <small id="emailHelp" className="form-text text-muted">
-        We'll never share your email with anyone else.
-        </small>
+          <label>Email</label>
+          <input
+            type="email"
+            className="form-control form-control-sm"
+            aria-describedby="emailHelp"
+            placeholder="name@example.com"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            We'll never share your email with anyone else.
+          </small>
 
-        <label>Password</label>
-        <input type="password" className="form-control form-control-sm" onChange={(e) => setPassword(e.target.value)}
-        required/>
+          <label>Password</label>
+          <input type="password" name="password" autoComplete="on" className="form-control form-control-sm" onChange={(e) => setPassword(e.target.value)} required />
         </div>
 
-          {submitted && !email && (
-            <div className="alert alert-danger" role="alert" style={{ textAlign: 'center' }}>
-              <FontAwesomeIcon icon={faExclamationCircle} />
-              <span>Please the email is required</span>
-            </div>
-          )}
+        {submitted && !email && (
+          <div className="alert alert-danger" role="alert" style={{ textAlign: 'center' }}>
+            <FontAwesomeIcon icon={faExclamationCircle} />
+            <span>Please the email is required.</span>
+          </div>
+        )}
+
+        {submitted && authentication.error && (
+          <div className="alert alert-danger" role="alert" style={{ textAlign: 'center' }}>
+            <span>Email or password is incorrect.</span>
+          </div>
+        )}
 
         <div className="login-buttons">
-        {authentication.loading &&
-        <img className={'loading'} alt='loading'
-             src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA=="/>
-        }
-        <button type="submit" className="btn btn-sm">
-        Login
-        </button>
-        <p className="forgot-password text-right">
-        New user? <Link to={'/register'}>Register here</Link>
-        </p>
+        {authentication.loading ? (
+              <div className="spinner-border spinner-border-sm text-secondary float-center" role="status">
+                <span className="sr-only">Loading...</span>
+              </div>
+            ) : null}
+          <button type="submit" className="btn btn-sm">
+            Login
+          </button>
+          <p className="forgot-password text-right">
+            New user? <Link to={'/register'}>Register here</Link>
+          </p>
         </div>
-        </form>
-      </div>
+      </form>
+    </div>
   );
 }
 
